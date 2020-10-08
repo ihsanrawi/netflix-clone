842341c2553385ce2f31b287e2976c06ae0f3e6d
Ihsan <ihsanrawi@gmail.com>
2020-10-08 15:44:37 +0800

    Jumbotron Component
    Created Jumbotron component

---
+++ src/components/jumbotron/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Container, Image, Inner, Item, Pane, Title, SubTitle } from "./styles/jumbotron";
+
+type JumbotronBaseProps = {
+  children?: React.ReactNode;
+};
+
+interface JumbotronProps extends JumbotronBaseProps {
+  direction: string;
+}
+
+export default function Jumbotron({ children, direction = "row" }: JumbotronProps) {
+  return (
+    <Item direction={direction}>
+      <Inner>{children}</Inner>
+    </Item>
+  );
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }: JumbotronBaseProps) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }: JumbotronBaseProps) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }: JumbotronBaseProps) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }: JumbotronBaseProps) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+interface JumbotronImageProps extends JumbotronBaseProps {
+  src: string;
+  alt: string;
+}
+
+Jumbotron.Image = function JumbotronImage({ children, src, alt }: JumbotronImageProps) {
+  return <Image {...{ src, alt }}>{children}</Image>;
+};
