18e284835525a954398c8312fd24ec3b34778589
Ihsan <ihsanrawi@gmail.com>
2020-10-09 16:06:31 +0800

    React Router Routes
    Created dummy pages for the routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import * as ROUTES from "./constants/routes";
-import Home from "./pages/home";
+import { Browse, Home, SignIn, SignUp } from "./pages";
 
 export default function App() {
   return (
     <Router>
       <Route exact path={ROUTES.HOME} component={Home} />
-      <Route exact path={ROUTES.BROWSE} component={Home} />
-      <Route exact path={ROUTES.SIGN_IN} component={Home} />
-      <Route exact path={ROUTES.SIGN_UP} component={Home} />
+      <Route exact path={ROUTES.BROWSE} component={Browse} />
+      <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
+      <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
     </Router>
   );
 }
