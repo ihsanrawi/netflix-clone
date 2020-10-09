7f5c409a046d2a41bd2a78fc250705a012e141b7
Ihsan <ihsanrawi@gmail.com>
2020-10-09 16:02:43 +0800

    React router
    initial setup of react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import * as ROUTES from "./constants/routes";
+import Home from "./pages/home";
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer/>
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ROUTES.HOME} component={Home} />
+      <Route exact path={ROUTES.BROWSE} component={Home} />
+      <Route exact path={ROUTES.SIGN_IN} component={Home} />
+      <Route exact path={ROUTES.SIGN_UP} component={Home} />
+    </Router>
   );
 }
---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { FaqsContainer, FooterContainer, JumbotronContainer } from "../containers";
+
+export default function Home() {
+  return (
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  );
+}
