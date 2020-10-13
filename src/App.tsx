a5776639e82a49ade149b3a2de2e7da7383d3597
Ihsan <ihsanrawi@gmail.com>
2020-10-13 15:30:34 +0800

    Routes
    Created protected routes and redirect user based on logged In status

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 import * as ROUTES from "./constants/routes";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 import { Browse, Home, SignIn, SignUp } from "./pages";
 
 export default function App() {
+  const user = null;
+
   return (
     <Router>
-      <Route exact path={ROUTES.HOME} component={Home} />
-      <Route exact path={ROUTES.BROWSE} component={Browse} />
-      <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
-      <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <SignIn />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <SignUp />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
