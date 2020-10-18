698d011840e477cd8851729b1a1a6c68743af2e2
Ihsan <ihsanrawi@gmail.com>
2020-10-18 21:31:40 +0800

    Set Browsse page
    Set Browse page, Added Feature, Search, dropdown for browse header styling

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
 
-import { Background, ButtonLink, Container, Logo } from "./styles/header";
+import {
+  Background,
+  ButtonLink,
+  Container,
+  Logo,
+  Feature,
+  Text,
+  FeatureCallOut,
+  Link,
+  Group,
+  Picture,
+  Profile,
+  Dropdown,
+  Search,
+  SearchIcon,
+  SearchInput,
+} from "./styles/header";
 
 interface HeaderBaseProps {
   children?: any;
@@ -12,8 +28,57 @@ export default function Header({ bg = true, children, ...restProps }: HeaderBase
   return bg ? <Background {...restProps}>{children}</Background> : children;
 }
 
+Header.Feature = function HeaderFeature({ children, ...restProps }: HeaderBaseProps) {
+  return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }: HeaderBaseProps) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }: HeaderBaseProps) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }: HeaderBaseProps) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }: HeaderBaseProps) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
+Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps }) {
+  const [searchActive, setSearchActive] = useState(false);
+  return (
+    <Search {...restProps}>
+      <SearchIcon onClick={() => setSearchActive((searchActive) => !searchActive)}>
+        <img src="/images/icons/search.png" alt="Search" />
+      </SearchIcon>
+      <SearchInput
+        value={searchTerm}
+        onChange={({ target }) => setSearchTerm(target.value)}
+        placeholder="Search films and series"
+        active={searchActive}
+      />
+    </Search>
+  );
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
 Header.Frame = function HeaderFrame({ children, ...restProps }: HeaderBaseProps) {
   return <Container {...restProps}>{children}</Container>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restProps }: HeaderBaseProps) {
+  return <Group {...restProps}>{children}</Group>;
 };
 
 Header.Logo = function HeaderLogo({ to, ...restProps }: HeaderBaseProps) {
